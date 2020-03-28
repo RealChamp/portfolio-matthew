1f38ee6882f860d8935981a5a4bc15d17d3283d4
Matvey <matvey.w2015@mail.ru>
2020-03-28 16:56:07 +0400

    some little fixes

--- src/js/index.js
+++ src/js/index.js
@@ -1,79 +1,71 @@
-( function($) {
+(function ($) {
 
-    $(function () {
-        $(window).on('load', () => {
-    
-            // Preloader
-    
-            $('.loader').fadeOut(1000);
-            let wow = new WOW({
-                offset: 150,
-                mobile: false,
-            })
-    
-            wow.init();
-        });
-    
-        // End of Preloader
-    
-    
-    
-        // SideBar Menu
-        $('.navbar-toggle').on('click', () => {
-            $('body').removeClass('menu-is-closed').addClass('menu-is-opened');
-        });
-    
-        $('.close-menu, .click-capture, .menu-list li a').on('click', () => {
-            $('body').removeClass('menu-is-opened').addClass('menu-is-closed');
-            $('.menu-list ul').slideUp(300);
-        });
-    
-        $('.menu-list li a').on('click', () => {
-            $('.menu-list li').removeClass('active');
-            $(this).closest('li').addClass('active');
-        });
-    
-        $('.col-resume').on('mouseover', () => {
-            $('.section-bg.mask').addClass('hide');
-        });
-    
-        $('.col-resume').on('mouseleave', () => {
-            $('.section-bg.mask').removeClass('hide');
-        });
-    
-        // End of menu SideBar
-    
-        // Owl Carousel
-    
-        if ($('.owl-carousel').length > 0) {
-            $('.review-carousel').owlCarousel({
-                dots: true,
-                nav: false,
-                navText: [],
-                responsiveRefreshRate: 0,
-                responsive: {
-                    0: {
-                        items: 1
-                    },
-                    720: {
-                        items: 1
-                    },
-                    1280: {
-                        items: 1
-                    }
-                },
-            });
-        }
-    
-        // End of Carousel
-    
-        // Sections
-    
-    
-    
-        //End of Sections
-    
+
+    $(window).on('load', () => {
+        $('.loader').fadeOut(1000);
+        let wow = new WOW({
+            offset: 150,
+            mobile: false,
+        })
+
+        wow.init();
     });
-    
+
+
+    $('.navbar-toggle').on('click', () => {
+        $('body').removeClass('menu-is-closed').addClass('menu-is-opened');
+    });
+
+    $('.close-menu, .click-capture, .menu-list li a').on('click', () => {
+        $('body').removeClass('menu-is-opened').addClass('menu-is-closed');
+        $('.menu-list ul').slideUp(300);
+    });
+
+    $('.menu-list li a').on('click', () => {
+        $('.menu-list li').removeClass('active');
+        $(this).closest('li').addClass('active');
+    });
+
+    $('.col-resume').on('mouseover', () => {
+        $('.section-bg.mask').addClass('hide');
+    });
+
+    $('.col-resume').on('mouseleave', () => {
+        $('.section-bg.mask').removeClass('hide');
+    });
+
+    // End of menu SideBar
+
+    // Owl Carousel
+
+    if ($('.owl-carousel').length > 0) {
+        $('.review-carousel').owlCarousel({
+            dots: true,
+            nav: false,
+            navText: [],
+            responsiveRefreshRate: 0,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                720: {
+                    items: 1
+                },
+                1280: {
+                    items: 1
+                }
+            },
+        });
+    }
+
+    // End of Carousel
+
+    // Sections
+
+
+
+    //End of Sections
+
+
 
 })(jQuery);
